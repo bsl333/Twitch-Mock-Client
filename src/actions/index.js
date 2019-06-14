c43d235fba3c752bbf0b1ae96cf9d3f846fa3735
Branden <b.lowe333@gmail.com>
2019-06-13 23:46:34 -0700

    created generic modal via portals, added delete functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -50,6 +50,7 @@ const editStream = (id, formValues) => async dispatch => {
 const deleteStream = id => async dispatch => {
   await streams.delete(`streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push('/');
 };
 
 export {
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Modal from '../Modal';
+import history from '../../history';
+import { deleteStream, fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-  return <div>StreamDelete!!</div>;
+
+class StreamDelete extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  onDeleteClicked = () => {
+    this.props.deleteStream(this.props.stream.id);
+  }
+
+  renderActions = () => (
+    <>
+      <div className="ui negative button" onClick={this.onDeleteClicked}>Delete</div>
+      <Link to="/" className="ui button" >Cancel</Link>
+    </>
+  )
+
+  renderContent() {
+    if(!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+
+    return (
+      <>
+        Are you sure you want to delete stream with title: <strong style={{textTransform: 'capitalize'}}>{this.props.stream.title}</strong>
+      </>
+    );
+  }
+
+  render() {
+    return (
+        <Modal
+          title="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+    );
+  }
 }
 
-export default StreamDelete;
+const mapStateToProps = ({ streams }, ownProps) => {
+  return {
+    stream: streams[ownProps.match.params.id]
+  };
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
